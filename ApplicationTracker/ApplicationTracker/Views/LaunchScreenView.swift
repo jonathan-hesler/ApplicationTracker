e33a8dfc36cfabff58c5b7a61de5199476eb5756
Jonathan Hesler <jeh8443@rit.edu>
2025-08-18 20:56:35 -0400

    Basic login view/launch view ui

--- ApplicationTracker/ApplicationTracker/Views/LaunchScreenView.swift
+++ ApplicationTracker/ApplicationTracker/Views/LaunchScreenView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct LaunchScreenView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                //Login form
+                Form {
+                    TextField("example@email.com", text: $email)
+                    SecureField("password", text: $password)
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Login")
+                    })
+                    .background(Color.blue)
+                    .foregroundStyle(.white)
+                }
+                
+                //Signup button
+                NavigationLink("Create an Account", destination: SignupView())
+            }
         }
-        .padding()
     }
 }
 
